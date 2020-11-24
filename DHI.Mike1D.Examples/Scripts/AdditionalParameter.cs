96a61e87a7c7df6913a4ab450cb819dc1443a448
Jesper Grooss <jgr@dhigroup.com>
2020-11-24 10:52:20 +0100

    Script: Add additional parameter

---
+++ DHI.Mike1D.Examples/Scripts/AdditionalParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DHI.Mike1D.Generic;
+using DHI.Mike1D.Mike1DDataAccess;
+using DHI.Mike1D.Plugins;
+using DHI.Mike1D.ResultDataAccess;
+
+namespace DHI.Mike1D.Examples.Scripts
+{
+  /// <summary>
+  /// Script showing how to add an additional parameter to the MIKE 1D data object
+  /// </summary>
+  public class AdditionalParameter
+  {
+    /// <summary>
+    /// Add additional parameter
+    /// </summary>
+    /// <param name="mike1DData"></param>
+    [Script]
+    public void AddAdditionalParameter(Mike1DData mike1DData)
+    {
+      mike1DData.AdditionalData.Add("AllowPMMultipleTailNodes", true);
+    }
+  }
+}
