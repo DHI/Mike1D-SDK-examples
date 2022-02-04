c9c7e4c9cfdc70db5de75d97c7d467c5b8070550
JesperGr <jgr@dhigroup.com>
2022-02-04 14:57:43 +0100

    Added script: BasinConeAreaInterpolation.cs

---
+++ DHI.Mike1D.Examples/Scripts/BasinConeAreaInterpolation.cs
@@ -0,0 +1,30 @@
+﻿using DHI.Mike1D.Mike1DDataAccess;
+using DHI.Mike1D.NetworkDataAccess;
+using DHI.Mike1D.Plugins;
+
+namespace DHI.Mike1D.Examples.Scripts
+{
+  public class BasinConeAreaInterpolation
+  {
+    /// <summary>
+    /// Setup all Basin to interpolate the area between level assuming
+    /// the basin has a conical shape, and interpolation is on
+    /// cone radii instead of cone area.
+    /// </summary>
+    /// <param name="mike1DData"></param>
+    [Script]
+    public void AddAdditionalParameter(Mike1DData mike1DData)
+    {
+      if (!mike1DData.UseHD)
+        return;
+
+      foreach (INode node in mike1DData.Network.Nodes)
+      {
+        IBasin basin = node as IBasin;
+        if (basin == null) continue;
+
+        basin.Geometry.AreaInterpolation = false;
+      }
+    }
+  }
+}
