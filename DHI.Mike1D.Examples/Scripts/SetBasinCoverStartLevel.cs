dd1c89386a216426297ec736e7b81a86b52239f5
LineKarinM <75069676+LineKarinM@users.noreply.github.com>
2025-04-07 14:43:32 +0200

    Add script to set the cover start level to the default value from version 2025 and older: ground level - 0.25 (#29)
    Co-authored-by: Line Karin Mortensen <likm@dhigroup.com>

---
+++ DHI.Mike1D.Examples/Scripts/SetBasinCoverStartLevel.cs
@@ -0,0 +1,33 @@
+﻿using DHI.Mike1D.Mike1DDataAccess;
+using DHI.Mike1D.NetworkDataAccess;
+using DHI.Mike1D.Plugins;
+
+namespace DHI.Mike1D.Examples.Scripts
+{
+  /// <summary>
+  /// Sets the start level of basin covers to the default value
+  /// used in version 2025 and earlier.
+  /// </summary>
+  public class SetBasinCoverStartLevel
+  {
+    private double _coverStartLevel = -0.25;
+    
+    /// <summary>
+    /// Method called when Mike1DData object has been loaded.
+    /// </summary>
+    [Script]
+    public void Initialize(Mike1DData mike1DData)
+    {
+      var nodes = mike1DData.Network.Nodes;
+
+      foreach (var node in nodes)
+      {
+        if (node is Basin basin && basin.Cover is INormalCover)
+        {
+          double groundLevel = basin.GroundLevel;
+          basin.Cover.WaterLevelStart = groundLevel + _coverStartLevel;
+        }
+      }
+    }
+  }
+}
